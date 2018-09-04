bc5e20a16707fcacdb48f26ab6676951bfbb67a5
henriquegallodias <henrique0521@hotmail.com>
2018-09-03 22:05:24 -0300

    aulastring java

---
+++ aulaStringss/HYee.java
@@ -0,0 +1,14 @@
+package ATIVIDADESLIDE;
+
+public class HYee {
+   
+	public static void main (String[] args) {
+		String str = "JAVA É JAVA ";
+		String xyz = new String ("JAVA É JAVA ");
+		if( str == xyz) System.out.println("Sim");
+		 else System.out.println ("CERTO");
+		 
+	}
+		
+	} 
+
